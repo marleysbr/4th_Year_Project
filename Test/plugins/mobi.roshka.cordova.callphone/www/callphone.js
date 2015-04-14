fbe2fd94f40072ff0890b8f25c9218aed3b56cdc
x00093357 <x00093357@ittd.ie>
2015-04-14 23:20:15 +0100

    Renamed project to ChurchFinderIreland

--- Test/plugins/mobi.roshka.cordova.callphone/www/callphone.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var callphone = {
-    call:function(success, error, number) {
-        exec(success, error, "CallPhone", "callNumber", [number]);
-    }
-};
-
-module.exports = callphone;
