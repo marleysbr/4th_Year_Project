d73d3bd053e0e42af1cc49f6db7caaccfe231614
x00093357 <x00093357@ittd.ie>
2015-04-15 15:10:44 +0100

    Added Daily Readings to Website

--- ChurchFinderIrelandWebsite/js/index.js
+++ ChurchFinderIrelandWebsite/js/index.js
@@ -98,3 +98,113 @@ var bindMarkerEvents = function(marker) {
         getDetailsFromMap(marker.get("id"));
     });
 };
+
+function getChurches() {
+    if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Chrome, Opera, Safari
+        var xmlhttp = new XMLHttpRequest();
+    }
+    else {  // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.async = false;
+    xmlhttp.open("GET", "http://www.churchfinderireland.com/xmlDatabase.php", false);
+    xmlhttp.send();
+    var xmlDoc = xmlhttp.responseXML;
+
+    var churchLength = xmlDoc.getElementsByTagName("Church").length;
+
+    for(var i = 0; i < churchLength; i++) {
+
+        var churchID = xmlDoc.getElementsByTagName("id")[i].childNodes[0].nodeValue;
+        var churchName = xmlDoc.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+        var churchCity = xmlDoc.getElementsByTagName("city")[i].childNodes[0].nodeValue;
+        var churchCounty = xmlDoc.getElementsByTagName("county")[i].childNodes[0].nodeValue;
+
+        $("#result_list").append("<a href='#' class='list-group-item'><b><h4>" + churchName + "</h4></b><p> - <b>City:</b> " + churchCity +" - <b>County:</b> " + churchCounty + "</p></a>");
+
+    }
+
+    /*$('.test').click(function() {
+     getDetails($(this).attr("id"));
+     }); */
+}
+
+function getNearMass() {
+
+    $('#result_listNear').empty();
+
+    // Try HTML5 geolocation
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+
+            if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Chrome, Opera, Safari
+                var xmlhttp = new XMLHttpRequest();
+            }
+            else {  // code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.async = false;
+            xmlhttp.open("GET", "http://www.churchfinderireland.com/xmlDatabase.php", false);
+            xmlhttp.send();
+            var xmlDoc = xmlhttp.responseXML;
+
+            var churchLength = xmlDoc.getElementsByTagName("Church").length;
+
+            for(i = 0; i < churchLength; i++) {
+
+                var churchID = xmlDoc.getElementsByTagName("id")[i].childNodes[0].nodeValue;
+                var churchName = xmlDoc.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+                var churchCity = xmlDoc.getElementsByTagName("city")[i].childNodes[0].nodeValue;
+                var churchCounty = xmlDoc.getElementsByTagName("county")[i].childNodes[0].nodeValue;
+
+                var churchCoordinates = xmlDoc.getElementsByTagName("coordinates")[i].childNodes[0].nodeValue;
+                var churchLat = churchCoordinates.split(',')[0].split(' ').pop();
+                var churchLong = churchCoordinates.substr(churchCoordinates.indexOf(",") + 1);
+                var churchLong = churchLong.replace(/\s+/g, '');
+
+                var distance = getDistanceFromLatLonInKm(churchLat, churchLong, position.coords.latitude, position.coords.longitude);
+
+                //churchArray.push({name: churchName, distance: distance1});
+                if(distance.toPrecision(4) < 25) {
+                    $("#result_listNear").append("<a href='#' class='list-group-item'><b><h4>" + churchName + "</h4></b>" +
+                    "<p><b>Distance:</b> " + distance.toPrecision(4) + "km</p>" + " </a>");
+                }
+
+            }
+
+            /*$('.test').click(function() {
+                getDetails2($(this).attr("id"));
+            });*/
+
+        });
+    }
+
+}
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
+
+function clearWatch1() {
+    if(watchID != null)
+    {
+        navigator.geolocation.clearWatch(watchID);
+        watchID = null;
+    }
+}
