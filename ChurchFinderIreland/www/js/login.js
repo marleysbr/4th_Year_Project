89b37f2d32c125c5eabdc12cb341815ab67bf842
x00093357 <x00093357@ittd.ie>
2015-04-21 14:09:34 +0100

    App
    Added comments to all files

--- ChurchFinderIreland/www/js/login.js
+++ ChurchFinderIreland/www/js/login.js
@@ -1,3 +1,4 @@
+//Check if the User is logged in by comparing the stored Values against the Database values
 function checkPreAuth() {
     var form = document.loginForm;
     if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
@@ -13,16 +14,16 @@ function checkPreAuth() {
     }
 }
 
+//Get the values from the TextFields, open a connection to the server and compare the values
 function logIn() {
-
     var un = document.loginForm.username.value;
     var pw = document.loginForm.password.value;
 
     if(un != "" && pw != "") {
-        if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Chrome, Opera, Safari
+        if (window.XMLHttpRequest) {
             xmlhttp = new XMLHttpRequest();
         }
-        else {  // code for IE6, IE5
+        else {
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
         xmlhttp.async = false;
@@ -36,6 +37,7 @@ function logIn() {
             username1 = xmlDoc.getElementsByTagName("username")[0].childNodes[0].nodeValue;
             password1 = xmlDoc.getElementsByTagName("password")[0].childNodes[0].nodeValue;
 
+            //Store the user details in the Local HTML 5 Database
             if(un == username1 && pw == password1) {
                 window.localStorage["username"] = un;
                 window.localStorage["password"] = pw;
@@ -58,6 +60,7 @@ function logIn() {
 
 }
 
+//Register the user
 function register() {
 
     var un = document.loginForm.username.value;
@@ -66,15 +69,15 @@ function register() {
     var em = document.loginForm.email.value;
 
     if(un != "" && pw != "" && pwc != "" && em != "") {
-        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i; //Check if email is in right format
         if(re.test(em) == true) {
             if (pw == pwc && pw.length >= 4) {
                 var data = "username=" + un + "&password=" + pw + "&email=" + em;
 
-                if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Chrome, Opera, Safari
+                if (window.XMLHttpRequest) {
                     xmlhttp = new XMLHttpRequest();
                 }
-                else {  // code for IE6, IE5
+                else {
                     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
                 }
 
@@ -88,6 +91,7 @@ function register() {
                 if (regDetail == 1) {
                     regState = xmlDoc.getElementsByTagName("Success")[0].childNodes[0].nodeValue;
 
+                    //If registration is successful, details are store in the HTML 5 Local Database
                     if (regState == "Completed") {
                         window.localStorage["username"] = un;
                         window.localStorage["password"] = pw;
